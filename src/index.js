e1b908aa36fba724645b7848d6af708d1ad7490b
Aio <aio392@DESKTOP-35OHEOS.localdomain>
2020-09-10 16:36:06 +0900

    Vanilla Redux Counter

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+number.innerText = 0;
 
-number.innerText = count;
+const ADD = "ADD";
+const MINUS = "MINUS";
+// keep 변수를 사용해서 디버거가 오류를 알려 줄 수 있도록 만든다.
 
-const updateText = () => {
-  number.innerText = count;
+const coountModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
+
+const countStore = createStore(coountModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  count++;
-  updateText();
+  countStore.dispatch({ type: ADD });
 };
+//  반드시 type 이어야함, 반드시 객체여햠, property는 소문자여도 됨
 const handleMinus = () => {
-  count--;
-  updateText();
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
